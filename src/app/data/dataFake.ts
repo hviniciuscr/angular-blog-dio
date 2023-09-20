cbfe75ddec2b411a5d65570a06df2c0a6d944add
Hebert Ramos <hvinicius87@gmail.com>
2023-09-20 18:10:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Sustentabilidade no centro das decisões globais",
+    "description": "Como o mundo tem pensado sobre o futuro e como tem agido para se tornar mais sustentável.",
+    "photoCover": "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/sustentabilidade.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Inteligências artificiais e meio ambiente",
+    "description": "olá",
+    "photoCover": "https://www.pensamentoverde.com.br/wp-content/uploads/2023/01/inteligencia-artificial-a-servico-da-sustentabilidade-nas-corporacoes.01.png"
+  },
+  {
+    "id": "3",
+    "title": "COP 30 será realizada em Belém",
+    "description": "olá",
+    "photoCover": "https://media.licdn.com/dms/image/D4E12AQGV1-Cc4ACx3w/article-cover_image-shrink_720_1280/0/1686261953147?e=2147483647&v=beta&t=W7ySQ7SDqZLNi91OD37qQ-hhtGdeO6M8n281mDBAEes"
+  },
+  {
+    "id": "4",
+    "title": "Análise do solo através de drones",
+    "description": "olá",
+    "photoCover": "https://alavoura.com.br/wp-content/uploads/2020/01/divulga%C3%A7%C3%A3o.jpg"
+  }
+]
